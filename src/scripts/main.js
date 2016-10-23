fc65c1139ae57ba1218c186eb60ada3099a0408d
Matthew Stratford <matthew@mstratford.net>
2016-10-23 15:57:48 +0100

    Added jQuery to make Current-Next boxes match in size to show thumbnail.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-console.log("hey");
+   
+    	$(function(){
+    		// On page load / resize, force now & next header boxes to be equal (sqaure) size.
+			var $el = $('.now-next-live');
+			var $el2 = $('.now-next-now');
+			var $el3 = $('.now-next-next');
+			var $window = $(window).on('resize', function(){
+			   var width = $el.outerWidth();
+			   $el.height(width);
+			   $el2.height(width);
+			   $el3.height(width);
+			}).trigger('resize'); //on page load
+
+		});
+
