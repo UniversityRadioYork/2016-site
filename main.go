509beaf2cc3c9ff352d845a410049cd03f2a2aca
Charles Pigott <charlespigott@googlemail.com>
2016-10-23 13:29:02 +0100

    Replace utils.GetConfigFromFile with toml.DecodeFile

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/UniversityRadioYork/2016-site/utils"
+	"github.com/BurntSushi/toml"
+	"github.com/UniversityRadioYork/2016-site/structs"
 	"github.com/UniversityRadioYork/2016-site/web"
 	"github.com/stretchr/graceful"
 	"log"
@@ -10,22 +11,20 @@ import (
 )
 
 func main() {
-
 	log.SetFlags(log.Llongfile)
 
-	//Get the config from the config.yaml file
-	config, err := utils.GetConfigFromFile("./config.toml")
+	config := &structs.Config{}
+	_, err := toml.DecodeFile("config.toml", config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	s, err := web.NewServer(config)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	l := fmt.Sprintf("%s:%d", config.Server.Address, config.Server.Port)
-
 	log.Printf("Listening on: %s", l)
-
 	graceful.Run(l, time.Duration(config.Server.Timeout), s)
-
 }
