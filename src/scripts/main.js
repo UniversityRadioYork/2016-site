4eac9d36c6b9608cde04f92d0bdf5c2fbb63502b
Matthew Stratford <matthew@mstratford.net>
2016-10-28 13:16:29 +0100

    Formatting Fixed, i'll have a go at trying to remove it shortly.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,12 @@
-   
-    	$(function(){
-    		// On page load / resize, force now & next header boxes to be equal (sqaure) size.
-			var $el = $('.now-next-live');
-			var $el2 = $('.now-next-now');
-			var $el3 = $('.now-next-next');
-			var $window = $(window).on('resize', function(){
-			   var width = $el.outerWidth();
-			   $el.height(width);
-			   $el2.height(width);
-			   $el3.height(width);
-			}).trigger('resize'); //on page load
-
-		});
-
+$(function() {
+  // On page load / resize, force now & next header boxes to be equal (sqaure) size.
+  var $el = $('.current-next-live');
+  var $el2 = $('.current-next-now');
+  var $el3 = $('.current-next-next');
+  var $window = $(window).on('resize', function() {
+    var width = $el.outerWidth();
+    $el.height(width);
+    $el2.height(width);
+    $el3.height(width);
+  }).trigger('resize'); //on page load
+});
