f6e8a97c3f406993b6c1bd6438787bbf9f94dba9
Matthew Stratford <matthew@mstratford.net>
2017-02-02 01:06:57 +0000

    Added basis for team pages

---
+++ controllers/team.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/UniversityRadioYork/2016-site/models"
+	"github.com/UniversityRadioYork/2016-site/structs"
+	"github.com/UniversityRadioYork/2016-site/utils"
+	"github.com/UniversityRadioYork/myradio-go"
+	"github.com/gorilla/mux"
+)
+
+// TeamController is the controller for the team information pages.
+type TeamController struct {
+	Controller
+}
+
+// NewTeamController returns a new TeamController with the MyRadio session s
+// and configuration context c.
+func NewTeamController(s *myradio.Session, c *structs.Config) *TeamController {
+	return &TeamController{Controller{session: s, config: c}}
+}
+
+// Get handles the HTTP GET request r for the user bio page, writing to w.
+func (teamC *TeamController) Get(w http.ResponseWriter, r *http.Request) {
+
+	teamM := models.NewTeamModel(teamC.session)
+
+	vars := mux.Vars(r)
+
+	id, _ := strconv.Atoi(vars["id"])
+
+	currentAndNext, err := teamM.Get(id)
+
+	if err != nil {
+		//@TODO: Do something proper here, render 404 or something
+		log.Println(err)
+		return
+	}
+
+	data := struct {
+		CurrentAndNext *myradio.CurrentAndNext
+	}{
+		CurrentAndNext: currentAndNext,
+	}
+
+	err = utils.RenderTemplate(w, teamC.config.PageContext, data, "team.tmpl", "elements/current_and_next.tmpl")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+}
---
+++ models/team.go
@@ -0,0 +1,26 @@
+package models
+
+import "github.com/UniversityRadioYork/myradio-go"
+
+// TeamModel is the model for the Team controller.
+type TeamModel struct {
+	Model
+}
+
+// NewTeamModel returns a new TeamModel on the MyRadio session s.
+func NewTeamModel(s *myradio.Session) *TeamModel {
+	return &TeamModel{Model{session: s}}
+}
+
+// Get gets the data required for the Team controller from MyRadio.
+//
+// On success, it returns the users name, bio, a list of officerships, their photo if they have one and nil
+// Otherwise, it returns undefined data and the error causing failure.
+
+func (m *TeamModel) Get(id int) (currentAndNext *myradio.CurrentAndNext, err error) {
+	currentAndNext, err = m.session.GetCurrentAndNext()
+	if err != nil {
+		return
+	}
+	return
+}
