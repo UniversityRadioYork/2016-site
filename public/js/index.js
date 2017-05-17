49fd2e1927b8924204448cf2aa082478c25290dd
Matthew Stratford <hi@mstratford.net>
2017-05-17 20:19:04 +0100

    Add youtube index slider js code

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+//Youtube slideshow for index page
+function onGoogleLoad() {
+  gapi.client.setApiKey('AIzaSyCzI5Gvpq1hPlfjvB81EVc0vLv_11eW7NI');
+  gapi.client.load('youtube', 'v3', function() {
+  var request = gapi.client.youtube.playlistItems.list({
+    part: 'snippet',
+    playlistId: 'PLsxUDJzU4VKDudlmlYb-ImFKreFKAfFln',
+    maxResults: 12
+  });
+
+  request.execute(function(response) {
+    for (var i = 0; i < response.items.length; i++) {
+      $('#youtube-video-slider').append( +
+        '<div class="col-sm-5 col-md-4 col-lg-3">' +
+          '<div class="thumbnail">' +
+            '<a href="//youtube.com/watch?v=' + response.items[i].snippet.resourceId.videoId + '" target="_blank">' +
+              '<img src="' + response.items[i].snippet.thumbnails.maxres.url +
+              '" alt="' + response.items[i].snippet.title + '">' +
+            '</a>' +
+          '</div>' +
+        '</div>');
+      }
+    });
+  });
+}
