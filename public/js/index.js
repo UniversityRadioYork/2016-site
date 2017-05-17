aef0fa0091070b5cb1d6e45ef7fd3cf4c40c1ea1
Matthew Stratford <hi@mstratford.net>
2017-05-17 21:02:10 +0100

    Actually fixed it.

--- public/js/index.js
+++ public/js/index.js
@@ -10,8 +10,7 @@ function onGoogleLoad() {
 
   request.execute(function(response) {
     for (var i = 0; i < response.items.length; i++) {
-      $('#youtube-video-slider').append( +
-        '<div class="col-10 col-sm-7 col-md-4 col-lg-3">' +
+      $('#youtube-video-slider').append('<div class="col-10 col-sm-7 col-md-4 col-lg-3">' +
           '<div class="thumbnail">' +
             '<a href="//youtube.com/watch?v=' + response.items[i].snippet.resourceId.videoId + '" target="_blank">' +
               '<img src="' + response.items[i].snippet.thumbnails.maxres.url +
