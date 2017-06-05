308197ff28d0ee6eda43eeedc6b877d18f943985
Tom Bellis <thomas.bellis@btinternet.com>
2017-06-05 08:48:59 +0100

    Added Site Description to config

--- structs/config.go
+++ structs/config.go
@@ -21,14 +21,15 @@ type Server struct {
 // This contains the website's long and short names, as well as a directory
 // of pages for navigation.
 type PageContext struct {
-	LongName     string `toml:"longName"`
-	ShortName    string `toml:"shortName"`
-	URLPrefix    string `toml:"urlPrefix"`
-	FullURL      string `toml:"fullURL"`
-	MainTwitter  string `toml:"mainTwitter"`
-	MainFacebook string `toml:"mainFacebook"`
-	NewsTwitter  string `toml:"newsTwitter"`
-	Pages        []Page
+	LongName        string `toml:"longName"`
+	ShortName       string `toml:"shortName"`
+	SiteDescription string `toml:"siteDescription"`
+	URLPrefix       string `toml:"urlPrefix"`
+	FullURL         string `toml:"fullURL"`
+	MainTwitter     string `toml:"mainTwitter"`
+	MainFacebook    string `toml:"mainFacebook"`
+	NewsTwitter     string `toml:"newsTwitter"`
+	Pages           []Page
 }
 
 // Page is a structure describing a page in the website navigation system.
