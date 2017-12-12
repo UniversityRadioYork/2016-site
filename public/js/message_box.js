ea49cb022f34113dfa3c5a987bc635fa6e5bb61d
Brooke <hatton.brooke@gmail.com>
2017-12-12 21:57:01 +0000

    fix some linting things

--- public/js/message_box.js
+++ public/js/message_box.js
@@ -1,34 +1,3 @@
-$(function() {
-    $('#studiomessage').submit(function(){
-        var currentShow = getCurrentShow();
-        var msg = $('#comments:first').val();
-        if (msg !== '' && typeof currentShow != 'undefined' && typeof currentShow.id == 'number') {
-            $('#comments').val("Sending...");
-            $.ajax({
-                type: 'PUT',
-                url: 'https://ury.org.uk/api/v2/timeslot/' + currentShow.id +'/sendmessage/?api_key=' + myRadioAPIKey,
-                data: { message: msg },
-                error: function() {
-                    messageSendError();
-                },
-                success: function() {
-                    $('#comments').val('');
-                    $('#comments').attr('placeholder','Message has been sent');
-                },
-                complete: function() {
-                    window.setTimeout(function() {
-                        $('#comments').val('');
-                        $('#comments').attr('placeholder','Why not send a message to the studio presenters? Have a bit of banter, share your opinions or simply request your favourite song?');
-                    }, 3000);
-                }
-            });
-        } else {
-            messageSendError();
-        }
-        return false;
-    });
-});
-
 function getCurrentShow() {
     $.getJSON("https://ury.org.uk/api/v2/timeslot/currentandnext/?api_key=" + myRadioAPIKey, (data) => {
         if (data.status === "OK") {
@@ -41,6 +10,38 @@ function getCurrentShow() {
 }
 
 function messageSendError() {
-    $('#comments').val("");
-    $('#comments').attr('placeholder', 'Message cannot be sent at this time. Is Jukebox on Air?')
+    $("#comments").val("");
+    $("#comments").attr("placeholder", "Message cannot be sent at this time. Is Jukebox on Air?")
 }
+
+
+$(function() {
+    $("#studiomessage").submit(function(){
+        var currentShow = getCurrentShow();
+        var msg = $("#comments:first").val();
+        if (msg !== "" && typeof currentShow != "undefined" && typeof currentShow.id == "number") {
+            $("#comments").val("Sending...");
+            $.ajax({
+                type: "PUT",
+                url: "https://ury.org.uk/api/v2/timeslot/" + currentShow.id +"/sendmessage/?api_key=" + myRadioAPIKey,
+                data: { message: msg },
+                error: () => {
+                    messageSendError();
+                },
+                success: () => {
+                    $("#comments").val("");
+                    $("#comments").attr("placeholder","Message has been sent");
+                },
+                complete: () => {
+                    window.setTimeout(function() {
+                        $("#comments").val("");
+                        $("#comments").attr("placeholder","Why not send a message to the studio presenters? Have a bit of banter, share your opinions or simply request your favourite song?");
+                    }, 3000);
+                }
+            });
+        } else {
+            messageSendError();
+        }
+        return false;
+    });
+});
