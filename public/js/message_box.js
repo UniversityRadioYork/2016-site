b25e3286689c9b4395fd6756ae9e979e2017b4ac
Brooke Hatton <Brookke@users.noreply.github.com>
2017-12-17 19:34:38 +0000

    Improve show pages and make the message box work (#74)
    * remove index schedule, to be added at a later date
    
    * Add credits with credit name to show page WIP
    
    * Improve scaling of show pages
    
    * correct tags
    
    * improve padding
    
    * improve how current and next area scales on mobile
    
    * Add new credits to the timeslot
    
    * give some love to codacy
    
    * Fix some codacy stuff
    
    * tabs to spaces
    
    * add message box
    
    * tidy up message box
    
    * use named returns
    
    * fix some linting things
    
    * add missing semi colon and change log to error
    
    * add comment for eslint
    
    this should work..maybe...idk
    
    * add eslint comment for console error
    
    * add info when tracklist is not there
    
    * move eslint to comment
    
    * update ES comment

---
+++ public/js/message_box.js
@@ -0,0 +1,50 @@
+/*global myRadioAPIKey:false*/
+
+function getCurrentShow() {
+    $.getJSON("https://ury.org.uk/api/v2/timeslot/currentandnext/?api_key=" + myRadioAPIKey, (data) => {
+        if (data.status === "OK") {
+            return data.payload.current;
+        } else {
+            // eslint-disable-next-line no-console
+            console.error("API Error: " + data.status + " - " + data.payload);
+            return;
+        }
+    });
+}
+
+function messageSendError() {
+    $("#comments").val("");
+    $("#comments").attr("placeholder", "Message cannot be sent at this time. Is Jukebox on Air?");
+}
+
+
+$(function() {
+    $("#studiomessage").submit(function(){
+        var currentShow = getCurrentShow();
+        var msg = $("#comments:first").val();
+        if (msg !== "" && typeof currentShow != "undefined" && typeof currentShow.id == "number") {
+            $("#comments").val("Sending...");
+            $.ajax({
+                type: "PUT",
+                url: "https://ury.org.uk/api/v2/timeslot/" + currentShow.id +"/sendmessage/?api_key=" + myRadioAPIKey,
+                data: { message: msg },
+                error: () => {
+                    messageSendError();
+                },
+                success: () => {
+                    $("#comments").val("");
+                    $("#comments").attr("placeholder","Message has been sent");
+                },
+                complete: () => {
+                    window.setTimeout(function() {
+                        $("#comments").val("");
+                        $("#comments").attr("placeholder","Why not send a message to the studio presenters? Have a bit of banter, share your opinions or simply request your favourite song?");
+                    }, 3000);
+                }
+            });
+        } else {
+            messageSendError();
+        }
+        return false;
+    });
+});
