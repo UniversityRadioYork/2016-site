172c6447de819b782f7612f904992e7e0adcf10f
Matthew Stratford <hi@mstratford.net>
2018-04-07 17:10:08 +0100

    Make friendly for non JS

--- public/js/show.js
+++ public/js/show.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    $("#timeslot-list").hide();
+    $("#timeslot-latest").show();
     var seasonTable = $("#seasons").DataTable({
         "ordering": false,
         "info":     false,
@@ -25,20 +27,49 @@ $(document).ready(function() {
 
         ]
     });
-    $(".timeslot-filter-season").on( "click", function () {
+
+    $.urlParam = function (name) {
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        if (results == null) {
+            return null;
+        }
+        else {
+            return decodeURI(results[1]) || 0;
+        }
+    }
+
+    var filterSeason = function(object) {
         $(".timeslot-filter-season").removeClass("active");
-        $(this).addClass("active");
+        $(object).addClass("active");
         $("#timeslot-latest").hide();
-        $("#timeslot-season-title").text("Season " + $(this).find("td:first-of-type").html())
+        $("#timeslot-season-title").text("Season " + $(object).find("td:nth-of-type(2)").html())
         timeslotTable
             .columns( 0 )
-            .search( $(this).data("seasonid") )
+            .search( $(object).data("seasonid") )
             .draw();
 
         $("#timeslot-list").show();
 
         $("#timeslot-list tr").on("click", function () {
-            window.location.href = $(this).find("a").attr("href");
+            window.location.href = $(object).find("a").attr("href");
         });
-    } );
-} );
+    };
+
+    $(".timeslot-filter-season").on( "click", function () {
+        filterSeason(this);
+    });
+
+    var getSeasonFilter = function() {
+        if ($.urlParam("seasonID") != null) {
+            var rowId = $("#seasons").dataTable()
+                .fnFindCellRowIndexes($.urlParam("seasonID"), 0);
+            var seasonTable = $("#seasons").DataTable()
+                .cell(7, 1)
+                .data('LOL')
+                .row(rowId).scrollTo();
+            //filterSeason($("tr[data-seasonid='" + $.urlParam("seasonID") + "']"));
+            //window.location.href = window.location.href.split('#')[0] + "#timeslot-list";
+        }
+    }
+    getSeasonFilter();
+});
