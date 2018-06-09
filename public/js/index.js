566b6cd778a98fd149fa52a46918034817d32f9e
Brooke <hatton.brooke@gmail.com>
2018-06-09 22:39:45 +0100

    update youtube container names

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ function onGoogleLoad() {
 
   request.execute(function(response) {
     for (var i = 0; i < response.items.length; i++) {
-      $('#youtube-video-slider').append('<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
+      $('#youtube-videos').append('<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
           '<div class="thumbnail">' +
             '<a href="//youtube.com/watch?v=' + response.items[i].snippet.resourceId.videoId + '" target="_blank">' +
               '<img src="' + response.items[i].snippet.thumbnails.maxres.url +
