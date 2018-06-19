29469f2c0b8b4f1fa8368cfbccd9929de677ea3a
Matthew Stratford <hi@mstratford.net>
2018-06-19 17:58:52 +0100

    Basic updating now & next.

--- public/js/index.js
+++ public/js/index.js
@@ -22,3 +22,75 @@ function onGoogleLoad() {
     });
   });
 }
+$(document).ready(function () {
+//Used for autoupdating the now and next.
+function updateShow() {
+  var data;
+  $.ajax({
+    dataType: "json",
+    url: "//ury.org.uk/api/v2/timeslot/currentandnext/?api_key=" + MyRadioAPIKey,
+    data: data,
+    success: function(data) {
+      var calcTime = function(timestamp) {
+        var date = new Date(timestamp * 1000);
+        // Hours part from the timestamp
+        var hours = date.getHours();
+        // Minutes part from the timestamp
+        var minutes = "0" + date.getMinutes();
+
+        return hours + ':' + minutes.substr(-2)
+      }
+      var makeContent = function(show) {
+        if (typeof show.title !== 'undefined') {
+          return "<h5 class=\"ellipsis\">"
+          + show.title
+          + "</h5>"
+          + "<h6>" + calcTime(show.start_time) +" - " + calcTime(show.end_time) + "</h6>";
+        } else {
+          return "<span>Looks like there is nothing on here.</span>"
+        }
+      }
+
+      if (typeof data.payload.current.url !== 'undefined') {
+
+        $(".current-and-next-now").replaceWith(
+          "<a class=\"current-and-next-now p-2 pt-3 px-3 p-md-3 p-lg-4 \" href="
+            + data.payload.current.url
+            + " title=\"View the show now on air.\">"
+            + "<h2>Now</h2>"
+            + makeContent(data.payload.current)
+            + "</a>");
+
+        $('#box-message').html('<h1>Send A Message</h1><span>Via the website, or text the studio</span><form action="https://ury.org.uk/schedule/message-current-show/" name="message" method="post"><textarea id="comments" name="comments" cols="30" rows="3" placeholder="Type your message here"></textarea><input type="submit" value="Send Message"></form><dl><dt>Text</dt><dd>07851 101 313</dd></dl>');
+      } else {
+        $(".current-and-next-now").replaceWith(
+          "<div class=\"current-and-next-now p-2 pt-3 px-3 p-md-3 p-lg-4 \" title=\"View the show now on air.\">"
+            + "<h2>Now</h2>"
+            + makeContent(data.payload.current)
+            + "</a>");
+      }
+      // Next show
+      if (typeof data.payload.next.url !== 'undefined') {
+        $(".current-and-next-next").replaceWith(
+          "<a class=\"current-and-next-next p-2 pt-3 px-3 p-md-3 p-lg-4 \" href="
+            + data.payload.nexturl
+            + " title=\"View the show up next.\">"
+            + "<h2>Next</h2>"
+            + makeContent(data.payload.next)
+            + "</a>");
+      } else {
+        $(".current-and-next-next").replaceWith(
+          "<div class=\"current-and-next-next p-2 pt-3 px-3 p-md-3 p-lg-4 \" title=\"View the show up next.\">"
+            + "<h2>Next</h2>"
+            + makeContent(data.payload.next)
+            + "</a>");
+      }
+
+      $(".current-and-next-img img").attr('src', "//ury.org.uk" + data.payload.current.photo);
+    }
+  });
+  setInterval(updateShow, 300000);
+}
+
+updateShow();
+});
