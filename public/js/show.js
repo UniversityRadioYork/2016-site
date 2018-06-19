a426ba55d75ce46aef61b922764476d236271f74
Matthew Stratford <hi@mstratford.net>
2018-06-19 12:57:20 +0100

    Move other JS to separate file.

--- public/js/show.js
+++ public/js/show.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+
+  $.fn.dataTable.Api.register('row().show()', function() {
+    var page_info = this.table().page.info();
+    // Get row index
+    var new_row_index = this.index();
+    // Row position
+    var row_position = this.table().rows()[0].indexOf( new_row_index );
+    // Already on right page ?
+    if( row_position >= page_info.start && row_position < page_info.end ) {
+        // Return row object
+        return this;
+    }
+    // Find page number
+    var page_to_display = Math.floor( row_position / this.table().page.len() );
+    // Go to that page
+    this.table().page( page_to_display );
+    // Return row object
+    return this;
+  });
+
   $("#timeslot-list").hide();
   $("#timeslot-latest").show();
 
