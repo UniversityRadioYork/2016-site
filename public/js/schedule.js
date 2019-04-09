c5aafd762f87263455b086ebff955f266dcea169
Matthew Stratford <hi@mstratford.net>
2019-04-10 00:12:42 +0100

    Fix Variable Assigned to Object Injection Sink

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -9,7 +9,15 @@ function zpad(n, width) {
 }
 
 function jumpToNow(disableMove = false) {
-  let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let daysOfWeek = {
+    0: "Sunday",
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday"
+  };
   let d = new Date();
   let weekdayIndex = d.getDay();
   let hour = d.getHours();
