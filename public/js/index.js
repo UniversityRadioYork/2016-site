ee27dbc44f8cce6269cfb8f07d0af1090e1654f1
Marks Polakovs <github@markspolakovs.me>
2020-04-04 19:05:24 +0200

    add ISTORN countdown

--- public/js/index.js
+++ public/js/index.js
@@ -66,6 +66,64 @@ function getYoutubeFeed(playlistid, results, htmlid)
   });
 }
 
+const isToday = () => {
+  // 12 feb 2020
+  someDate = new Date('2020-02-12')
+  let today = new Date();
+  today = someDate.getDate() == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear();
+  return today;
+};
+
+// CIN countdown
+function cinCounter() {
+  if (isCIN) {
+    const now = new Date();
+    const cin = new Date("2020-02-12T19:00:00Z");
+
+    const diffSeconds = (cin - now) / 1000;
+    const timerSeconds = (diffSeconds % 60).toFixed(0).padStart(2, "0");
+    const timerMinutes = Math.floor(diffSeconds % 3600 / 60).toFixed(0).padStart(2, "0");
+    const timerHours = Math.floor(diffSeconds % 86400 / 3600).toFixed(0).padStart(2, "0");
+    const timerDays = Math.floor(diffSeconds / 86400).toFixed(0);
+
+    if (false) {
+      document.getElementById("cinCountdown").innerText = "" + timerDays + " days " + timerHours + ":" + timerMinutes + ":" + timerSeconds;
+    } else {
+      document.getElementById("cinCountdownDays").innerText = timerDays;
+      document.getElementById("cinCountdownHours").innerText = timerHours;
+      document.getElementById("cinCountdownMinutes").innerText = timerMinutes;
+      document.getElementById("cinCountdownSeconds").innerText = timerSeconds;
+    }
+
+    window.setTimeout(cinCounter, 1000);
+  }
+}
+cinCounter();
+
+// ISTORN 2020 countdown
+function istorn2020Counter() {
+  if (isISTORN2020) {
+    const now = new Date();
+    const istorn2020 = new Date("2020-04-13T11:00:00Z");
+
+    const diffSeconds = (istorn2020 - now) / 1000;
+    const timerSeconds = (diffSeconds % 60).toFixed(0).padStart(2, "0");
+    const timerMinutes = Math.floor(diffSeconds % 3600 / 60).toFixed(0).padStart(2, "0");
+    const timerHours = Math.floor(diffSeconds % 86400 / 3600).toFixed(0).padStart(2, "0");
+    const timerDays = Math.floor(diffSeconds / 86400).toFixed(0);
+
+    document.getElementById("istornCountdownDays").innerText = timerDays;
+    document.getElementById("istornCountdownHours").innerText = timerHours;
+    document.getElementById("istornCountdownMinutes").innerText = timerMinutes;
+    document.getElementById("istornCountdownSeconds").innerText = timerSeconds;
+
+    window.setTimeout(istorn2020Counter, 1000);
+  }
+}
+istorn2020Counter();
+
 //Youtube slideshow for index page
 function onGoogleLoad() {
 
