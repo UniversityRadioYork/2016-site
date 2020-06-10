0d05666280a2df8e42cf932dd41aad71e1c8b2e9
Your name here <brookke@users.noreply.github.com>
2020-06-10 20:18:19 +0000

    Some fixes

--- public/js/index.js
+++ public/js/index.js
@@ -1,87 +1,115 @@
-function getYoutubeFeed(playlistid, results, htmlid)
-{
+function getYoutubeFeed(playlistid, results, htmlid) {
   gapi.client.setApiKey(youtubeAPIKey);
-  gapi.client.load('youtube', 'v3', function() {
-  
+  gapi.client.load("youtube", "v3", function() {
     var request = gapi.client.youtube.playlistItems.list({
-      part: 'snippet',
+      part: "snippet",
       playlistId: playlistid,
       maxResults: results
     });
 
     request.execute(function(response) {
       for (var i = 0; i < response.items.length; i++) {
-        $(htmlid).append('<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
-          '<div class="thumbnail">' +
-            '<a href="//youtube.com/watch?v=' + response.items[i].snippet.resourceId.videoId + '" target="_blank">' +
-              '<img src="' + response.items[i].snippet.thumbnails.maxres.url +
-              '" alt="' + response.items[i].snippet.title + '" class="img-fluid">' +
-            '</a>' +
-          '</div>' +
-        '</div>');
+        $(htmlid).append(
+          '<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
+            '<div class="thumbnail">' +
+            '<a href="//youtube.com/watch?v=' +
+            response.items[i].snippet.resourceId.videoId +
+            '" target="_blank">' +
+            '<img src="' +
+            response.items[i].snippet.thumbnails.maxres.url +
+            '" alt="' +
+            response.items[i].snippet.title +
+            '" class="img-fluid">' +
+            "</a>" +
+            "</div>" +
+            "</div>"
+        );
       }
-      if(isIndex && htmlid === "#sessions-videos") {
-        $(htmlid).append('<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
-          '<a class="ury-card sessions link" href=\'/ontap/\'>' +
+      if (isIndex && htmlid === "#sessions-videos") {
+        $(htmlid).append(
+          '<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
+            "<a class=\"ury-card sessions link\" href='/ontap/'>" +
             '<div class="ury-card-body">' +
-              '<div class="ury-card-lg-title">See more...</div>' +
-            '</div>' +
-          '</a>' +
-        '</div>');
+            '<div class="ury-card-lg-title">See more...</div>' +
+            "</div>" +
+            "</a>" +
+            "</div>"
+        );
       }
-      if(isOD && htmlid === "#sessions-videos") {
-        $(htmlid).append('<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
-          '<a class="ury-card sessions link" href=\"' + youtubeLink + '\">' +
+      if (isOD && htmlid === "#sessions-videos") {
+        $(htmlid).append(
+          '<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
+            '<a class="ury-card sessions link" href="' +
+            youtubeLink +
+            '">' +
             '<div class="ury-card-body">' +
-              '<div class="ury-card-lg-title">View more on Youtube...</div>' +
-            '</div>' +
-          '</a>' +
-        '</div>');
+            '<div class="ury-card-lg-title">View more on Youtube...</div>' +
+            "</div>" +
+            "</a>" +
+            "</div>"
+        );
       }
-      if(isIndex && htmlid === "#cin-videos") {
-        $(htmlid).append('<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
-          '<a class="ury-card cin link" href=\'/cin/\'>' +
+      if (isIndex && htmlid === "#cin-videos") {
+        $(htmlid).append(
+          '<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
+            "<a class=\"ury-card cin link\" href='/cin/'>" +
             '<div class="ury-card-body">' +
-              '<div class="ury-card-lg-title">See more...</div>' +
-            '</div>' +
-          '</a>' +
-        '</div>');
+            '<div class="ury-card-lg-title">See more...</div>' +
+            "</div>" +
+            "</a>" +
+            "</div>"
+        );
       }
-      if(isCIN && htmlid === "#cin-videos") {
-        $(htmlid).append('<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
-          '<a class="ury-card cin link" href=\"' + youtubeLink + '\">' +
+      if (isCIN && htmlid === "#cin-videos") {
+        $(htmlid).append(
+          '<div class="thumbnail-container col-10 col-sm-7 col-md-4 col-lg-3">' +
+            '<a class="ury-card cin link" href="' +
+            youtubeLink +
+            '">' +
             '<div class="ury-card-body">' +
-              '<div class="ury-card-lg-title">View more on Youtube...</div>' +
-            '</div>' +
-          '</a>' +
-        '</div>');
+            '<div class="ury-card-lg-title">View more on Youtube...</div>' +
+            "</div>" +
+            "</a>" +
+            "</div>"
+        );
       }
     });
-
   });
 }
 
 //Youtube slideshow for index page
 function onGoogleLoad() {
-  
-  if(isIndex) {
+  if (isIndex) {
     getYoutubeFeed(youtubeSessionsPlaylistID, 7, "#sessions-videos");
   }
-  if(isOD) {
+  if (isOD) {
     getYoutubeFeed(youtubeSessionsPlaylistID, 15, "#sessions-videos");
   }
-  if(isIndex) {
+  if (isIndex) {
     getYoutubeFeed(youtubeCINPlaylistID, 7, "#cin-videos");
   }
-  if(isCIN) {
+  if (isCIN) {
     getYoutubeFeed(youtubeCINPlaylistID, 15, "#cin-videos");
   }
 }
 
 //Comments Character Count
-document.getElementById("comments").onkeyup = function(){
-  var remaining = 1000 - document.getElementById("comments").value.length;
-  document.getElementById("charcount").style.display = (remaining<=100) ? "block":"none";
-  document.getElementById("charcount").style.color = (remaining<=10) ? "red":"black";
-  document.getElementById("charcount").innerHTML = remaining + " Characters Remaining";
+
+function updateMessageboxCharacterCount() {
+  var remaining =
+    document.getElementById("comments").maxLength -
+    document.getElementById("comments").value.length;
+  document.getElementById("charcount").style.color =
+    remaining <= 10 ? "red" : "black";
+  document.getElementById(
+    "charcount"
+  ).innerHTML = `${remaining} Characters Remaining`;
+}
+
+document.getElementById("comments").onkeyup = () => {
+  updateMessageboxCharacterCount();
 };
+
+$(document).ready(() => {
+  updateMessageboxCharacterCount();
+});
