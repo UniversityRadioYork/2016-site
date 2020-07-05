2b197e5c8d2878a7428a21f34343088133d43973
Marks Polakovs <marks@markspolakovs.me>
2020-07-05 13:43:33 +0200

    Redo schedule generation logic to make it a straight list

--- models/schedule.go
+++ models/schedule.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"net/url"
+	"sort"
 	"time"
 
 	"github.com/UniversityRadioYork/2016-site/structs"
@@ -20,11 +21,15 @@ func NewScheduleModel(s *myradio.Session) *ScheduleModel {
 	return &ScheduleModel{Model{session: s}}
 }
 
-// WeekSchedule gets the week schedule with ISO-8601 year year and week number week.
+// OLD_WeekSchedule gets the week schedule with ISO-8601 year year and week number week.
 // It also takes sustainer configuration, and a function to use for generating timeslot URLs.
-// On success, it returns the fully tabulated WeekSchedule for the given week.
+// On success, it returns the fully tabulated OLD_WeekSchedule for the given week.
 // Otherwise, it returns undefined data and the error causing failure.
-func (m *ScheduleModel) WeekSchedule(year, week int, sustainerConfig structs.SustainerConfig, timeslotURLBuilder func(*myradio.Timeslot) (*url.URL, error)) (*WeekSchedule, error) {
+func (m *ScheduleModel) WeekSchedule(
+	year, week int,
+	sustainerConfig structs.SustainerConfig,
+	timeslotURLBuilder func(*myradio.Timeslot) (*url.URL, error),
+) (*WeekSchedule, error) {
 	startDate, err := utils.IsoWeekToDate(year, week, time.Monday)
 	if err != nil {
 		return nil, err
@@ -55,7 +60,122 @@ func (m *ScheduleModel) WeekSchedule(year, week int, sustainerConfig structs.Sus
 		return nil, err
 	}
 
-	return tabulateWeekSchedule(weekStart, weekFinish, filled)
+	top, bot, err := calcScheduleBoundaries(filled, weekStart)
+
+	days, dayItems := m.divideItemsIntoDays(filled, weekStart, weekFinish, top, bot)
+
+	return &WeekSchedule{
+		Days:         days,
+		DayItems:     dayItems,
+		EarliestHour: top,
+		LatestHour:   bot,
+	}, nil
+}
+
+func (m *ScheduleModel) divideItemsIntoDays(
+	items []*ScheduleItem,
+	start, finish time.Time,
+	dayTop, dayBot utils.StartOffset,
+) ([]time.Time, map[time.Time][]*ScheduleItem) {
+
+	// These numbers _should_ have come from calcScheduleBoundaries, so they _should_ be legit
+	lastHour, err := dayBot.ToHour()
+	if err != nil {
+		panic(err)
+	}
+	firstHour, err := dayTop.ToHour()
+	if err != nil {
+		panic(err)
+	}
+
+	days := make([]time.Time, 0)
+	dayItems := make(map[time.Time][]*ScheduleItem)
+
+	for d := start; d.Before(finish); d = d.AddDate(0, 0, 1) {
+		days = append(days, d)
+		dayItems[d] = make([]*ScheduleItem, 0)
+	}
+
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].Start.Before(items[j].Start)
+	})
+
+	// We now know the items are in chronological order.
+	today := start
+	for i, item := range items {
+		nextDay := today.AddDate(0, 0, 1)
+		if item.Start.After(nextDay) {
+			// The first item "today" is actually tomorrow - it means we've finished a day
+			today = nextDay
+			nextDay = today.AddDate(0, 0, 1)
+		}
+		// Check if this item straddles the day
+		if item.Finish.After(nextDay) {
+			// If so, add it to both today's and tomorrow's schedules
+			// Make a copy, so we can fiddle some fields
+			tomorrowsVersion := *item
+
+			// If it's a sustainer, extend it out - but only as much as needed.
+			if item.IsSustainer() {
+				item.Finish = time.Date(
+					item.Finish.Year(),
+					item.Finish.Month(),
+					item.Finish.Day(),
+					lastHour,
+					0,
+					0,
+					0,
+					item.Finish.Location(),
+				)
+				tomorrowsVersion.Start = time.Date(
+					item.Start.Year(),
+					item.Start.Month(),
+					item.Start.Day(),
+					firstHour,
+					0,
+					0,
+					0,
+					tomorrowsVersion.Start.Location(),
+				)
+				// Only add in tomorrow's version if there isn't already a show then
+				if i != len(items)-2 {
+					if tomorrowsVersion.Start != items[i+1].Start {
+						_, ok := dayItems[nextDay]
+						if ok {
+							dayItems[nextDay] = append(dayItems[nextDay], &tomorrowsVersion)
+						}
+					}
+				} else {
+					_, ok := dayItems[nextDay]
+					if ok {
+						dayItems[nextDay] = append(dayItems[nextDay], &tomorrowsVersion)
+					}
+				}
+			} else {
+				// If it's a real show, make it properly straddle.
+				item.ContinuesOnNextDay = true
+				item.Finish = nextDay
+				tomorrowsVersion.Start = nextDay
+				tomorrowsVersion.ContinuedFromPrevDay = true
+				// Sanity check
+				_, ok := dayItems[nextDay]
+				if ok {
+					dayItems[nextDay] = append(dayItems[nextDay], &tomorrowsVersion)
+				}
+			}
+		}
+
+		// Fun edge case
+		if item.IsSustainer() {
+			if item.Start.Before(item.Finish) /* Don't question it. */ {
+				dayItems[today] = append(dayItems[today], item)
+			}
+		} else {
+			dayItems[today] = append(dayItems[today], item)
+		}
+	}
+
+	return days, dayItems
 }
 
 // GetCurrentAndNext retrieves the data for the current and next show
