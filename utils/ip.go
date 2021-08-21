2b8221bfe921f3d08698a23007ee96d2986c0067
Marks Polakovs <marks@markspolakovs.me>
2021-08-21 12:09:38 +0100

    Initial short URLs support

---
+++ utils/ip.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"github.com/UniversityRadioYork/2016-site/structs"
+	"net"
+	"net/http"
+)
+
+func GetRequesterIP(c *structs.Config, r *http.Request) *net.IPAddr {
+	return nil
+}
