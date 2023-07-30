2541ccf151320369cae00c43a35f4f3666b8f1e2
Matt Windsor <mattwindsor91@gmail.com>
2023-07-30 13:38:13 +0100

    podcasts: Eagerly construct model
    It seems strange to construct the model afresh every request,
    when we have everything needed to construct it available in the ctor.

--- controllers/podcasts.go
+++ controllers/podcasts.go
@@ -15,18 +15,20 @@ import (
 // PodcastController is the controller for the URYPlayer Podcast pages.
 type PodcastController struct {
 	Controller
+	model *models.PodcastModel
 }
 
 // NewPodcastController returns a new PodcastController with the MyRadio session s
 // and configuration context c.
 func NewPodcastController(s *myradio.Session, c *structs.Config) *PodcastController {
-	return &PodcastController{Controller{session: s, config: c}}
+	return &PodcastController{
+		Controller: Controller{session: s, config: c},
+		model:      models.NewPodcastModel(s),
+	}
 }
 
 // GetAllPodcasts handles the HTTP GET request r for the all postcasts page, writing to w.
 func (podcastsC *PodcastController) GetAllPodcasts(w http.ResponseWriter, r *http.Request) {
-	podcastm := models.NewPodcastModel(podcastsC.session)
-
 	vars := mux.Vars(r)
 
 	pageNumber, _ := strconv.Atoi(vars["page"])
@@ -37,14 +39,14 @@ func (podcastsC *PodcastController) GetAllPodcasts(w http.ResponseWriter, r *htt
 	pageNumberNext := pageNumber + 1
 
 	//podcast page offset is indexed from 0, URL's are from 1.
-	podcasts, err := podcastm.GetAllPodcasts(10, pageNumber-1)
+	podcasts, err := podcastsC.model.GetAllPodcasts(10, pageNumber-1)
 	if podcasts == nil {
 		podcastsC.notFound(w, err)
 		return
 	}
 
 	//see if it's possible to load another podcast for a possible next page.
-	nextPodcasts, _ := podcastm.GetAllPodcasts(1, pageNumber)
+	nextPodcasts, _ := podcastsC.model.GetAllPodcasts(1, pageNumber)
 
 	var pageNext = false
 	if nextPodcasts != nil {
@@ -108,7 +110,7 @@ func (podcastsC *PodcastController) getPodcast(r *http.Request) (*myradio.Podcas
 		return nil, err
 	}
 
-	return models.NewPodcastModel(podcastsC.session).Get(id)
+	return podcastsC.model.Get(id)
 }
 
 func (podcastsC *PodcastController) renderPodcast(w http.ResponseWriter, podcast *myradio.Podcast, tmpl string) {
