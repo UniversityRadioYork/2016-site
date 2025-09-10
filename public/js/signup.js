0467dcf048139c4bae167d8d848feb4310844f9a
Ashhhleyyy <ash@ashhhleyyy.dev>
2025-09-10 19:23:09 +0100

    feat: more work on new signups

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,45 @@
+import { c, g, h, s, x } from './xeact.js';
+
 (function () {
-  const trainingContinueButton = document.getElementById('training-continue');
+  const trainingContinueButton = g('training-continue');
+
+  async function updateAvailableSlots() {
+    const resp = await fetch('https://ury.org.uk/api/v2/demo/listdemos/?api_key=' + MyRadioAPIKey);
+    const data = await resp.json();
+    const trainingsList = g('available-trainings');
+
+    document.querySelectorAll('.is-actual-training-slot').forEach(ele => ele.remove());
+
+    console.log(data.payload);
+    trainingsList.prepend(...data.payload.filter(demo => demo.presenterstatusid === 'Studio Trained').map(demo => 
+      h('label', {
+        className: 'card h4',
+        for: `training-${demo.demo_id}`,
+      }, [
+        h('div', {
+          className: 'card-body',
+        }, [
+          h('div', {
+            className: 'form-check',
+          }, [
+            h('input', {
+              className: 'form-check-input training-slot-select',
+              type: 'radio',
+              name: 'sessionid',
+              value: demo.demo_id,
+              id: `training-${demo.demo_id}`,
+            }),
+            h('div', {
+              className: 'form-check-label',
+              innerText: demo.demo_time,
+            }),
+          ]),
+        ])
+      ])
+    ));
+
+    disableTrainingContinue();
+  }
 
   function onTrainingSessionSelected() {
     console.log('onTrainingSessionSelected')
@@ -11,10 +51,16 @@
     }
   }
 
-  document.querySelectorAll('.training-slot-select').forEach(ele => ele.addEventListener('change', onTrainingSessionSelected));
+  function disableTrainingContinue() {
+    trainingContinueButton.classList.add('disabled');
+    trainingContinueButton.ariaDisabled = false;
+    trainingContinueButton.dataset.href = trainingContinueButton.href;
+    trainingContinueButton.removeAttribute('href');
+  }
 
-  trainingContinueButton.classList.add('disabled');
-  trainingContinueButton.ariaDisabled = false;
-  trainingContinueButton.dataset.href = trainingContinueButton.href;
-  trainingContinueButton.removeAttribute('href');
+  disableTrainingContinue();
+
+  s('.training-slot-select').forEach(ele => ele.addEventListener('change', onTrainingSessionSelected));
+
+  g('profile-continue').addEventListener('click', updateAvailableSlots);
 })();
